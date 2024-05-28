6339cd2def40208fbae181a26828f37c6d50dabf
yeeun <hannah001204@naver.com>
2024-05-28 15:22:53 +0900

    GenericAPIView , Mixins를 이용한 CRUD

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('',PostListCreateAPIView.as_view(),name='post-list-create'),
+    path('<int:pk>',PostRetrieveUpdateDestroyAPIView.as_view(),name='post-detail'),
+]
